7cfdf58ddd1b0c879a419aaf8e1ae1b5e43cd0f9
shellbye <444854713@qq.com>
2014-10-23 19:52:50 +0800

    simplified LuTestSearch.java

--- src/main/java/lucene/LuTestSearch.java
+++ src/main/java/lucene/LuTestSearch.java
@@ -21,37 +21,23 @@ import java.util.Date;
  */
 public class LuTestSearch {
     public static void main(String[] arg) throws Exception{
+        //使用LuTestIndex.java建立索引之后索引所在的目录（indexPath）
         String index = "/home/shellbye/result/";
         String field = "contents";
-        int repeat = 0;
-        String queries = null;
-        String queryString = null;
-
 
         IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(index)));
         IndexSearcher searcher = new IndexSearcher(reader);
-        // :Post-Release-Update-Version.LUCENE_XY:
         Analyzer analyzer = new StandardAnalyzer();
 
-        BufferedReader in = null;
-        if (queries != null) {
-            in = new BufferedReader(new InputStreamReader(new FileInputStream(queries), StandardCharsets.UTF_8));
-        } else {
-            in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-        }
-        // :Post-Release-Update-Version.LUCENE_XY:
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+
         QueryParser parser = new QueryParser(field, analyzer);
         while (true) {
-            if (queries == null && queryString == null) {                        // prompt the user
-                System.out.println("Enter query: ");
-            }
-
-            String line = queryString != null ? queryString : in.readLine();
-
+            System.out.println("Enter query: ");     // prompt the user
+            String line = in.readLine();
             if (line == null || line.length() == -1) {
                 break;
             }
-
             line = line.trim();
             if (line.length() == 0) {
                 break;
@@ -59,22 +45,9 @@ public class LuTestSearch {
 
             Query query = parser.parse(line);
             System.out.println("Searching for: " + query.toString(field));
-
-            if (repeat > 0) {                           // repeat & time as benchmark
-                Date start = new Date();
-                for (int i = 0; i < repeat; i++) {
-                    searcher.search(query, null, 100);
-                }
-                Date end = new Date();
-                System.out.println("Time: " + (end.getTime() - start.getTime()) + "ms");
-            }
-            Integer hitsPerPage = 10;
-            Boolean raw = false;
-            doPagingSearch(in, searcher, query, hitsPerPage, raw, false);
-
-            if (queryString != null) {
-                break;
-            }
+            // 每页显示的数量
+            Integer hitsPerPage = 5;
+            doPagingSearch(in, searcher, query, hitsPerPage);
         }
         reader.close();
     }
@@ -83,15 +56,11 @@ public class LuTestSearch {
      * This demonstrates a typical paging search scenario, where the search engine presents
      * pages of size n to the user. The user can then go to the next page if interested in
      * the next hits.
-     * <p/>
-     * When the query is executed for the first time, then only enough results are collected
-     * to fill 5 result pages. If the user wants to page beyond this limit, then the query
-     * is executed another time and all hits are collected.
      */
     public static void doPagingSearch(BufferedReader in, IndexSearcher searcher, Query query,
-                                      int hitsPerPage, boolean raw, boolean interactive) throws IOException {
+                                      int hitsPerPage) throws IOException {
 
-        // Collect enough docs to show 5 pages
+        // 至多搜索5页的结果
         TopDocs results = searcher.search(query, 5 * hitsPerPage);
         ScoreDoc[] hits = results.scoreDocs;
 
@@ -99,28 +68,12 @@ public class LuTestSearch {
         System.out.println(numTotalHits + " total matching documents");
 
         int start = 0;
-        int end = Math.min(numTotalHits, hitsPerPage);
 
         while (true) {
-            if (end > hits.length) {
-                System.out.println("Only results 1 - " + hits.length + " of " + numTotalHits + " total matching documents collected.");
-                System.out.println("Collect more (y/n) ?");
-                String line = in.readLine();
-                if (line.length() == 0 || line.charAt(0) == 'n') {
-                    break;
-                }
+            int end = Math.min(hits.length, start + hitsPerPage);
 
-                hits = searcher.search(query, numTotalHits).scoreDocs;
-            }
-
-            end = Math.min(hits.length, start + hitsPerPage);
-
+            // 遍历每一页
             for (int i = start; i < end; i++) {
-                if (raw) {                              // output raw format
-                    System.out.println("doc=" + hits[i].doc + " score=" + hits[i].score);
-                    continue;
-                }
-
                 Document doc = searcher.doc(hits[i].doc);
                 String path = doc.get("path");
                 if (path != null) {
@@ -132,12 +85,11 @@ public class LuTestSearch {
                 } else {
                     System.out.println((i + 1) + ". " + "No path for this document");
                 }
-
+                System.out.println("doc=" + hits[i].doc + " score=" + hits[i].score);
             }
 
-            if (!interactive || end == 0) {
-                break;
-            }
+
+            if (end == 0) break;
 
             if (numTotalHits >= end) {
                 boolean quit = false;
