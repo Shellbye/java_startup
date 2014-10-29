f45730ef7b42612b302e6001d455992e8fba6999
shellbye <shellbye.com@gmail.com>
2014-10-29 10:35:56 +0800

    mongo

---
+++ src/main/java/mongoDb/MongoTest.java
@@ -0,0 +1,14 @@
+package mongoDb;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shellbye.com@gmail.com
+ * Date: 2014/10/29
+ * Time: 10:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class MongoTest {
+    public static void main(String[] args){
+        System.out.println("yes");
+    }
+}
