63111155d41e270c2666c3c60f5d9eb7e9634254
shellbye <444854713@qq.com>
2014-10-30 17:01:34 +0800

    just need doRank() to finish

--- src/main/java/mongoDb/ZhiHuUserTag.java
+++ src/main/java/mongoDb/ZhiHuUserTag.java
@@ -1,28 +1,46 @@
 package mongoDb;
 
 import com.mongodb.*;
+
 import java.security.MessageDigest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by shellbye on 10/30/14.
  */
 public class ZhiHuUserTag {
-    public static void main(String[] args){
-        try{
-            MongoClient mongoClient = new MongoClient( "192.168.2.221" , 27017 );
-            DB db = mongoClient.getDB( "user" );
+    static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("Enter the main");
+        try {
+            MongoClient mongoClient = new MongoClient("192.168.2.221", 27017);
+            DB db = mongoClient.getDB("user");
 
             DBCollection zhihuuserCollection = db.getCollection("zhihuuser");
             DBCollection zhihu_questionCollection = db.getCollection("zhihu_question");
+            DBCollection zhihu_user_question_tagCollection = db.getCollection("zhihu_user_question_tag");
             DBCursor zhihuuserCursor = zhihuuserCollection.find();
 
+
             // todo while
-            for(int i = 0; i < 10; i++){
+            for (int i = 0; i < 5; i++) {
+                logger.info("Enter the user-for loop and process number " + i);
                 // 1.在zhihuuser中获取用户的id，即name字段
                 DBObject currentZhiHuUser = zhihuuserCursor.next();
-                String name = (String)currentZhiHuUser.get("name");
-                // todo record
-//                System.out.println(name);
+                String name = (String) currentZhiHuUser.get("name");
+
+                BasicDBObject queryOfUserTag = new BasicDBObject();
+                queryOfUserTag.put("name", name);
+                DBCursor zhihu_user_question_tagCursor = zhihu_user_question_tagCollection.find(queryOfUserTag);
+                DBObject zhihu_user_tag_Object;
+                if (zhihu_user_question_tagCursor.hasNext()) {
+                    zhihu_user_tag_Object = zhihu_user_question_tagCursor.next();
+                } else {
+                    zhihu_user_tag_Object = create(zhihu_user_question_tagCollection, name);
+                }
 
 
                 // 2.通过name在zhihu_answers中找到用户的所有问题
@@ -30,45 +48,100 @@ public class ZhiHuUserTag {
                 BasicDBObject query = new BasicDBObject();
                 query.put("name", name);
                 DBObject zhiHuAnswer = zhihu_answersCollection.findOne(query);
-//                todo record
-//                System.out.println(zhiHuAnswer.get("_id"));
-                DBObject zhihuQuestions = (DBObject)zhiHuAnswer.get("questions");
-                for(String key : zhihuQuestions.keySet()){
-                    String question = (String)zhihuQuestions.get(key);
-                    String idOfQuestion = md5(question);
-                    // todo record
-//                    System.out.println(idOfQuestion);
 
-                    // 用于验证md5的找法是正确的
+                update(zhihu_user_question_tagCollection, zhihu_user_tag_Object,
+                        "zhihu_answers_id", zhiHuAnswer.get("_id"));
+
+                DBObject zhihuQuestions = (DBObject) zhiHuAnswer.get("questions");
+                // 3.遍历所有的问题的内容，通过对内容进行md5找到真正的问题
+                for (String key : zhihuQuestions.keySet()) {
+                    logger.info("Enter the question-for loop and process question " + zhihuQuestions.get("_id"));
+                    String question = (String) zhihuQuestions.get(key);
+                    String idOfQuestion = md5(question);
+
+
+                    update(zhihu_user_question_tagCollection, zhihu_user_tag_Object,
+                            "zhihu_question_id", zhihuQuestions.get("_id"));
+                    update(zhihu_user_question_tagCollection, zhihu_user_tag_Object,
+                            "zhihu_question_md5_id", idOfQuestion);
+
+
                     BasicDBObject queryOfQuestion = new BasicDBObject();
                     queryOfQuestion.put("id", idOfQuestion);
                     DBCursor zhihu_questionCursor = zhihu_questionCollection.find(queryOfQuestion);
-                    if (zhihu_questionCursor.hasNext()){
-                        // 3.找到问题对应的tags
+
+                    if (zhihu_questionCursor.hasNext()) {
+                        // 4.找到真正的问题，以及其对应的tags
                         DBObject questionObject = zhihu_questionCursor.next();
-                        String tags = (String)questionObject.get("tags");
-                        System.out.println(tags);
-                        for(String tag : tags.split(",")){
-                            System.out.println(tag.trim());
+                        String tags = (String) questionObject.get("tags");
+                        for (String tag : tags.split(",")) {
+                            logger.info("Enter the tags-for loop and the tags is " + tags);
+                            String trimmedTag = tag.trim();
+                            if (tagAlreadyExists(zhihu_user_tag_Object, trimmedTag)) {
+                                tagCountIncrease(zhihu_user_question_tagCollection, zhihu_user_tag_Object, trimmedTag);
+                            } else {
+                                addTag(zhihu_user_question_tagCollection, zhihu_user_tag_Object, trimmedTag);
+                            }
                         }
                     }
                 }
             }
-        }catch(Exception e){
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
     }
 
-    public static String md5(String in) throws Exception{
+    public static DBObject create(DBCollection collection, String name) {
+        logger.info("create name " + name);
+        BasicDBObject basicDBObject = new BasicDBObject("name", name).append("tags_info", new BasicDBObject());
+        collection.insert(basicDBObject);
+        return basicDBObject;
+    }
+
+    public static void update(DBCollection collection, DBObject dbObject, String key, Object value) {
+        logger.info("update " + dbObject.get("_id") + " key " + key + " to " + value);
+        dbObject.put(key, value);
+        collection.save(dbObject);
+    }
+
+    public static boolean tagAlreadyExists(DBObject dbObject, String tag) {
+        BasicDBObject tagsInfo = (BasicDBObject) dbObject.get("tags_info");
+        return tagsInfo.get(tag) != null;
+    }
+
+    public static void tagCountIncrease(DBCollection collection, DBObject dbObject, String tag) {
+        logger.info("tag " + tag + " increase");
+        BasicDBObject tagsInfo = (BasicDBObject) dbObject.get("tags_info");
+        BasicDBObject t = (BasicDBObject) tagsInfo.get(tag);
+        t.put("count", (int) t.get("count") + 1);
+        doRank();
+    }
+
+    public static void addTag(DBCollection collection, DBObject dbObject, String tag) {
+        logger.info("add tag " + tag);
+        BasicDBObject tagsInfo = (BasicDBObject) dbObject.get("tags_info");
+            if (tag.contains(".")) {
+                tagsInfo.put(tag.replace(".", "\\u002e"), new BasicDBObject("count", 1).append("rank", -1));
+            } else {
+                tagsInfo.put(tag, new BasicDBObject("count", 1).append("rank", -1));
+            }
+            collection.save(dbObject);
+    }
+
+    public static void doRank() {
+        // todo
+    }
+
+    public static String md5(String in) throws Exception {
         MessageDigest md = MessageDigest.getInstance("MD5");
         md.update(in.getBytes("UTF-8"));
 
         byte byteData[] = md.digest();
 
         StringBuilder hexString = new StringBuilder();
-        for (int i=0;i<byteData.length;i++) {
-            String hex=Integer.toHexString(0xff & byteData[i]);
-            if(hex.length()==1) hexString.append('0');
+        for (int i = 0; i < byteData.length; i++) {
+            String hex = Integer.toHexString(0xff & byteData[i]);
+            if (hex.length() == 1) hexString.append('0');
             hexString.append(hex);
         }
 
